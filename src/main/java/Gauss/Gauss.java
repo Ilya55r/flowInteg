702a899ef3d6997f9438ed51d8102fea9cc08c87
Ilya <ilya-55-mck@mail.ru>
2019-10-07 16:14:30 +0600

    Calculation

--- src/main/java/Gauss/Gauss.java
+++ src/main/java/Gauss/Gauss.java
@@ -14,8 +14,18 @@ public class Gauss{
 	public double[][] getMatrix(){
 		return matrix;
 	}
-	public static void main(String[] args){
+	
+	public void calculate(){
+		for (int i = 0;i<varNum;i++)
+			for (int j=j+1; j<varNum;j++){
+				double num = array[j][i]/(matrix[i][i]);
+				
+				//for (int k= 0; k<varNum;k++)
+					//array[j][k] = array[j][k]/(array[i][k]*num);
+				
+				
+			}
 		
 	}
-	
+		
 }
