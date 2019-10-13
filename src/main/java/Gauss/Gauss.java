fffca44bf0deeac7a5b7d7d8d4322c420cee0bef
Ilya <ilya-55-mck@mail.ru>
2019-10-13 10:36:13 +0600

    Calcultaion 2

--- src/main/java/Gauss/Gauss.java
+++ src/main/java/Gauss/Gauss.java
@@ -1,3 +1,5 @@
+package linearEquationSystem;
+
 
 public class Gauss{
 	private int varNum;
@@ -17,14 +19,14 @@ public class Gauss{
 	
 	public void calculate(){
 		for (int i = 0;i<varNum;i++)
-			for (int j=j+1; j<varNum;j++){
+			for (int j = j+1; j < varNum; j++){
 				double num = array[j][i]/(matrix[i][i]);
 				
-				//for (int k= 0; k<varNum;k++)
-					//array[j][k] = array[j][k]/(array[i][k]*num);
-				
-				
+				for (int k = j; k < varNum; k++)
+					array[j][k] = array[j][k]-(array[i][k]*num);
 			}
+			
+			
 		
 	}
 		
