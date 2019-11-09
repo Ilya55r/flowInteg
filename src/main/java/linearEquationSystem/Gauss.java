39de004a506cf19369842a6ba5bce2daccb70465
Ilya <ilya-55-mck@mail.ru>
2019-11-09 17:15:50 +0600

    JUnit5

--- src/main/java/linearEquationSystem/Gauss.java
+++ src/main/java/linearEquationSystem/Gauss.java
@@ -27,26 +27,26 @@ public class Gauss{
 		for (int i = 0;i<varNum;i++)
 			for (int j = i+1; j < varNum; j++){
 				double num = matrix[j][i]/(matrix[i][i]);
-				
-				for (int k = j; k < varNum; k++)
+				for (int k = 0; k < varNum+1; k++)
 					matrix[j][k] = matrix[j][k]-(matrix[i][k]*num);
 			}
 			
-	for (int i = varNum - 1; i >= 0; i--){
+		for (int i = varNum - 1; i >= 0; i--){
 		
-		result[i] = matrix[i][varNum] / matrix[i][i];
+			result[i] = matrix[i][varNum] / matrix[i][i];
 		
-		for (int j = i - 1; j >=0; j--){
-			matrix[j][varNum]-= matrix[j][i] * result[i];
-			matrix[j][i] = 0;
+			for (int j = i - 1; j >=0; j--){
+				matrix[j][varNum]-= matrix[j][i] * result[i];
+				matrix[j][i] = 0;
+			}
+			
+	
 		}
-			
-	
-	}
 	
 			
 			
 		
 	}
+	
 		
 }
